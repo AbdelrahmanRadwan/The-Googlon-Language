139bb2470cc898bed7573987c6fe199429225e05
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2018-10-26 19:17:19 +0200

    Fixed a bug in the utils

--- helper_functions/utils.py
+++ helper_functions/utils.py
@@ -1,4 +1,15 @@
+import sys
+import os
+
+import json
+from pathlib import Path
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 from helper_functions.constants import (googlon_to_english_dict, googlon_to_numbers_dict)
+
+
+TEST_DIR = str(Path(__file__).parents[1]) + "/test/"
 
 
 def is_foo_letter(character):
@@ -63,3 +74,14 @@ def word_to_number(word: str):
 def is_pretty_number(word: str):
     number = word_to_number(word=word)
     return number >= 81827 and number % 3 == 0
+
+
+def read_json_content(file_name=TEST_DIR + "test_cases.json"):
+    with open(file_name, 'r') as file:
+        data = json.load(file)
+    return data
+
+
+def write_json_content(data, file_name=TEST_DIR+"test_results/results.json"):
+    with open(file_name, 'w') as outfile:
+        json.dump(data, outfile, sort_keys=True, indent=2, ensure_ascii=False)
